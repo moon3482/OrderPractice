ca1961b93887c762e1b395e342774139e0aa0655
Charlie_Moon <moon3482@cau.ac.kr>
2023-08-29 01:31:55 +0900

    인트로 화면 구현

--- app/src/main/java/com/example/myapplication/intro/IntroFragment.kt
+++ app/src/main/java/com/example/myapplication/intro/IntroFragment.kt
@@ -1,32 +1,56 @@
 package com.example.myapplication.intro
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentIntroBinding
+import com.example.myapplication.list.ListFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class IntroFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = IntroFragment()
-    }
-
+    private var _binding: FragmentIntroBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "FragmentIntroBinding is Null"
+        }
     private lateinit var viewModel: IntroViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentIntroBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(IntroViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[IntroViewModel::class.java]
+        with(binding) {
+            lifecycleOwner = this@IntroFragment
+            toListPage.setOnClickListener {
+                parentFragmentManager
+                    .beginTransaction()
+                    .replace(
+                        R.id.fragmentContainerView,
+                        ListFragment.newInstance(),
+                    ).commit()
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance() = IntroFragment()
     }
 
 }
