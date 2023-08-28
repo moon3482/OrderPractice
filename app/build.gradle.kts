45837efb289e927bd92d4f001b45a446af8c5c25
Charlie_Moon <moon3482@cau.ac.kr>
2023-08-29 01:26:03 +0900

    의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.recyclerview:recyclerview:1.3.1")
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
