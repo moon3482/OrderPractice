c4cd7a3e2fc49ddf06d63c50bf9cd49b7ac6cf61
문창규 <ckmoon@smartscore.kr>
2023-08-29 21:16:08 +0900

    Serializable 적용

--- app/src/main/java/com/example/myapplication/model/ListMenu.kt
+++ app/src/main/java/com/example/myapplication/model/ListMenu.kt
@@ -1,26 +1,28 @@
 package com.example.myapplication.model
 
-sealed class ListMenu(
-    open val name: String,
-    open val price: Int,
-) {
+import java.io.Serializable
+
+sealed class ListMenu : Serializable {
+    abstract val name: String
+    abstract val price: Int
+
     data class Coffee(
         override val name: String,
         override val price: Int,
-    ) : ListMenu(name = name, price = price)
+    ) : ListMenu()
 
     data class Ade(
         override val name: String,
         override val price: Int,
-    ) : ListMenu(name = name, price = price)
+    ) : ListMenu()
 
     data class Tea(
         override val name: String,
         override val price: Int,
-    ) : ListMenu(name = name, price = price)
+    ) : ListMenu()
 
     data class Desert(
         override val name: String,
         override val price: Int,
-    ) : ListMenu(name = name, price = price)
+    ) : ListMenu()
 }
