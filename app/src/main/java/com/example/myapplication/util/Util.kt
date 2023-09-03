cd24002054e19aa756f11ab647a327617b113bf8
문창규 <ckmoon@smartscore.kr>
2023-09-03 15:55:38 +0900

    메뉴 옵션 문자열 변환 추가

--- app/src/main/java/com/example/myapplication/util/Util.kt
+++ app/src/main/java/com/example/myapplication/util/Util.kt
@@ -1,6 +1,28 @@
 package com.example.myapplication.util
 
+import com.example.myapplication.model.IcePortion
+import com.example.myapplication.model.OrderMenu
 import java.text.DecimalFormat
 
 val decimalFormat = DecimalFormat("#,###")
 fun Int.toKRWString(): String = "${decimalFormat.format(this)}원"
+fun OrderMenu.toOptionString(): String{
+    val optionList = mutableListOf<String>()
+    when (isHot) {
+        true -> optionList.add("HOT")
+        false -> optionList.add("ICE")
+        null -> Unit
+    }
+    when (isCaffeine) {
+        true -> optionList.add("카페인")
+        false -> optionList.add("디카페인")
+        null -> Unit
+    }
+    when (icePortion) {
+        IcePortion.SMALL -> optionList.add("얼음(적게)")
+        IcePortion.MEDIUM -> optionList.add("얼음(보통)")
+        IcePortion.LARGE -> optionList.add("얼음(많이)")
+        null -> Unit
+    }
+    return optionList.joinToString(separator = "/")
+}
