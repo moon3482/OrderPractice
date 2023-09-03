096f484065ad9fe30e6e9184922ffc901ce02912
문창규 <ckmoon@smartscore.kr>
2023-09-04 00:29:43 +0900

    BindingAdapter 수정

--- app/src/main/java/com/example/myapplication/BindingAdapter.kt
+++ app/src/main/java/com/example/myapplication/BindingAdapter.kt
@@ -1,25 +1,27 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.detail.DetailUiEvent
-import com.example.myapplication.list.adapter.ListAdapter
+import com.example.myapplication.intro.IntroUiEvent
+import com.example.myapplication.list.ListUiEvent
+import com.example.myapplication.list.adapter.ListMenuAdapter
+import com.example.myapplication.list.adapter.MenuGroupAdapter
+import com.example.myapplication.list.adapter.MenuGroupDecoration
 import com.example.myapplication.model.ListMenu
+import com.example.myapplication.model.MenuGroup
 import com.example.myapplication.model.OrderMenu
+import com.example.myapplication.order.OrderUiEvent
 import com.example.myapplication.util.toKRWString
 
 @BindingAdapter("bind:price")
 fun TextView.setPrice(price: Int) {
-    this.text = price.toKRWString()
-}
-
-@BindingAdapter("bind:menuList")
-fun RecyclerView.setMenuList(menuList: List<ListMenu>?) {
-    val list = menuList ?: emptyList()
-    (this.adapter as? ListAdapter)?.setMenuList(list)
+    text = price.toKRWString()
 }
 
 @BindingAdapter("bind:isVisible")
@@ -27,13 +29,60 @@ fun View.isVisible(isVisible: Boolean) {
     this.isVisible = isVisible
 }
 
-@BindingAdapter("bind:uiEvent", "bind:orderMenu")
-fun TextView.setSendOrder(
+@BindingAdapter("bind:groupList")
+fun RecyclerView.setGroupList(groupList: List<MenuGroup>?) {
+    val list = groupList ?: emptyList()
+    addItemDecoration(MenuGroupDecoration(Color.parseColor("#EDF1F4"),50F))
+    (adapter as? MenuGroupAdapter)?.setGroupList(list)
+}
+
+@BindingAdapter("bind:onClickChildItem")
+fun RecyclerView.setOnClickChildItem(listUiEvent: ListUiEvent?) {
+    (adapter as? MenuGroupAdapter)?.setChildUiEvent(listUiEvent)
+}
+
+@BindingAdapter("bind:menuList")
+fun RecyclerView.setMenuList(menuList: List<ListMenu>?) {
+    val list = menuList ?: emptyList()
+    (adapter as? ListMenuAdapter)?.setMenuList(list)
+}
+
+@BindingAdapter("bind:onClickItem")
+fun RecyclerView.setOnClickItem(listUiEvent: ListUiEvent?) {
+    (adapter as? ListMenuAdapter)?.setOnClickMenu {
+        listUiEvent?.onClickMenu(it)
+    }
+}
+
+@BindingAdapter("bind:onClickNext")
+fun View.setOnClickNext(uiEvent: IntroUiEvent?) {
+    setOnClickListener {
+        uiEvent?.onClickNext()
+    }
+}
+
+@BindingAdapter("bind:onClickOrder", "bind:orderMenu")
+fun View.setOnClickOrder(
     uiEvent: DetailUiEvent?,
-    orderMenu: OrderMenu?
+    orderMenu: OrderMenu?,
 ) {
-    this.setOnClickListener {
-        if (orderMenu != null)
-            uiEvent?.moveToOrder(orderMenu)
+    setOnClickListener {
+        orderMenu?.let { orderMenu ->
+            uiEvent?.onClickOrder(orderMenu)
+        }
+    }
+}
+
+@BindingAdapter("bind:onClickClose")
+fun View.setOnClickClose(orderUiEvent: OrderUiEvent?) {
+    setOnClickListener {
+        orderUiEvent?.onClickClose()
+    }
+}
+
+@BindingAdapter("bind:onClickBack")
+fun Toolbar.setOnClickClose(onClickClose: (() -> Unit)?) {
+    setNavigationOnClickListener {
+        onClickClose?.invoke()
     }
 }
