0cf784025cc89a0cae428410165787e874a5c0bd
문창규 <ckmoon@smartscore.kr>
2023-09-03 15:42:45 +0900

    ListMenu data class로 변경

--- app/src/main/java/com/example/myapplication/model/ListMenu.kt
+++ app/src/main/java/com/example/myapplication/model/ListMenu.kt
@@ -2,27 +2,8 @@ package com.example.myapplication.model
 
 import java.io.Serializable
 
-sealed class ListMenu : Serializable {
-    abstract val name: String
-    abstract val price: Int
-
-    data class Coffee(
-        override val name: String,
-        override val price: Int,
-    ) : ListMenu()
-
-    data class Ade(
-        override val name: String,
-        override val price: Int,
-    ) : ListMenu()
-
-    data class Tea(
-        override val name: String,
-        override val price: Int,
-    ) : ListMenu()
-
-    data class Desert(
-        override val name: String,
-        override val price: Int,
-    ) : ListMenu()
-}
+data class ListMenu(
+    val name:String,
+    val price:Int,
+    val menuType: MenuType,
+)
