ba3c0f78772df00ab0fa33fc9ca56478c05eecc6
문창규 <ckmoon@smartscore.kr>
2023-09-03 15:54:08 +0900

    Order 화면 수정
    레이아웃 추가
    주문 메뉴 표시
    뒤로가기, 닫기 버튼 클릭시 인트로 화면 이동

--- app/src/main/java/com/example/myapplication/order/OrderFragment.kt
+++ app/src/main/java/com/example/myapplication/order/OrderFragment.kt
@@ -5,7 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentOrderBinding
+import com.example.myapplication.intro.IntroFragment
+import com.example.myapplication.model.OrderMenu
+import com.example.myapplication.util.toKRWString
+import com.example.myapplication.util.toOptionString
 
 class OrderFragment : Fragment() {
     private var _binding: FragmentOrderBinding? = null
@@ -23,9 +31,44 @@ class OrderFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            val name = arguments?.getString("menuName")
+            val price = arguments?.getInt("menuPrice")
+            val option = arguments?.getString("menuOption")
+            menuName.text = name
+            manuPrice.text = price?.toKRWString()
+            menuOption.text = option
+            orderToolbar.setNavigationOnClickListener {
+                moveToIntro()
+            }
+            close.setOnClickListener {
+                moveToIntro()
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun moveToIntro() {
+        parentFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        parentFragmentManager.commit {
+            replace<IntroFragment>(
+                containerViewId = R.id.fragmentContainerView,
+            )
+        }
+    }
+
+    companion object {
+        fun arguments(orderMenu: OrderMenu): Bundle = Bundle().apply {
+            putString("menuName", orderMenu.name)
+            putInt("menuPrice", orderMenu.price)
+            putString("menuOption", orderMenu.toOptionString())
+        }
     }
 
 }
